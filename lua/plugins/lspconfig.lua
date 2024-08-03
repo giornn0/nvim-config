0bbcb8a289de8a8a28af0f8bfd0501e9794d069b
giornn0 <rusty.arrebol@proton.me>
2024-08-03 01:56:32 -0300

    feat: elixir working lsp -- with use of LazyNvim config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -71,6 +71,33 @@ return {
             new_config.cmd = angular_cmd
           end,
         },
+        elixirls = {
+          cmd = { "elixir-ls" },
+          keys = {
+            {
+              "<leader>cp",
+              function()
+                local params = vim.lsp.util.make_position_params()
+                LazyVim.lsp.execute({
+                  command = "manipulatePipes:serverid",
+                  arguments = { "toPipe", params.textDocument.uri, params.position.line, params.position.character },
+                })
+              end,
+              desc = "To Pipe",
+            },
+            {
+              "<leader>cP",
+              function()
+                local params = vim.lsp.util.make_position_params()
+                LazyVim.lsp.execute({
+                  command = "manipulatePipes:serverid",
+                  arguments = { "fromPipe", params.textDocument.uri, params.position.line, params.position.character },
+                })
+              end,
+              desc = "From Pipe",
+            },
+          },
+        },
         tailwindcss = tailwindcss,
       })
       return opts
