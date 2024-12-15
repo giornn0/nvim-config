f6610fe86c78712856939e1e3f74e741967fa963
giornn0 <rusty.arrebol@proton.me>
2024-12-15 20:37:56 -0300

    feat: catpuccin colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,5 @@
 return {
   {
-    "folke/tokyonight.nvim",
-    opts = function()
-      return {
-        transparent = true,
-        styles = {
-          sidebars = "transparent",
-          floats = "transparent",
-        },
-      }
-    end,
-  },
-  {
     "craftzdog/solarized-osaka.nvim",
     lazy = true,
     priority = 1000,
@@ -24,5 +12,33 @@ return {
         },
       }
     end,
+  },
+  {
+    -- Plugin for the Catppuccin color scheme
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = true, -- Load this plugin immediately
+    opts = {
+      transparent_background = true, -- Enable transparent background
+      flavour = "mocha", -- Set the flavor of the color scheme
+    },
+    integrations = {
+      cmp = true, -- Enable integration with nvim-cmp
+      gitsigns = true, -- Enable integration with gitsigns
+      nvimtree = true, -- Enable integration with nvim-tree
+      treesitter = true, -- Enable integration with treesitter
+      notify = false, -- Disable integration with nvim-notify
+      mini = {
+        enabled = true, -- Enable mini plugin integration
+        indentscope_color = "", -- Set indentscope color (empty means default)
+      },
+      -- Additional plugin integrations can be found in the documentation
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
   },
 }
