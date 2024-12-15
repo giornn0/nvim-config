a1b75333278a6f94ef46035320f613cf3a6c9427
giornn0 <rusty.arrebol@proton.me>
2024-12-15 20:38:58 -0300

    feat: info for options config + fold(start)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,14 +1,18 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
---
-vim.cmd([[let &t_Cs = "\[4:3m"]])
-vim.cmd([[let &t_Ce = "\[4:0m"]])
-
-vim.wo.relativenumber = false
+--INFO: PHP lsp
 vim.g.lazyvim_php_lsp = "intelephense"
+
+--INFO: Fold config
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldenable = false
+
+--INFO: General config
+vim.wo.relativenumber = false
+
+--INFO: JS Linter
 -- Set to false to disable auto format
-vim.g.lazyvim_eslint_auto_format = true
+vim.g.lazyvim_eslint_auto_format = false
+--INFO: JS Formatter
 -- Enable the option to require a Prettier config file
 -- If no prettier config file is found, the formatter will not be used
-vim.g.lazyvim_prettier_needs_config = false
+vim.g.lazyvim_prettier_needs_config = true
