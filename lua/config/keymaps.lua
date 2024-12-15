d79015ac88f07b1aaaf80d405440cdb8afcfd61b
giornn0 <rusty.arrebol@proton.me>
2024-12-15 20:39:22 -0300

    feat: new line mapping

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = LazyVim.safe_keymap_set
+
+map("n", "<leader>Nn", "A<CR>", { desc = "New line from end", silent = true })
+map("n", "<leader>Nb", "i<CR>", { desc = "New line in place", silent = true })
