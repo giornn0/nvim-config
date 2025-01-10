1412ea4a0e728b345c0dafe2a424d87acf0cfa17
giornn0 <rusty.arrebol@proton.me>
2025-01-10 04:21:37 -0300

    feat: zig lang

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,6 +27,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.zig" },
 
     -- Ts/Js World
     { import = "lazyvim.plugins.extras.lang.typescript" },
