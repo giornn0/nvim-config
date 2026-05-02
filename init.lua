4122927b793a183776651721adef6274a03127be
giornn0 <rusty.arrebol@proton.me>
2026-05-01 23:59:11 -0300

    feat: clean providers

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_python3_provider = 0
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.filetype.add({
